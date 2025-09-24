4c21093d8ee29fb37bb84745a307ac32f8341723
th1nksnow <buffallo654@yandex.ru>
2025-09-25 01:27:59 +0300

    Add SetAddress func

--- parcel.go
+++ parcel.go
@@ -61,7 +61,12 @@ func (s ParcelStore) SetStatus(number int, status string) error {
 func (s ParcelStore) SetAddress(number int, address string) error {
 	// реализуйте обновление адреса в таблице parcel
 	// менять адрес можно только если значение статуса registered
-
+	_, err := s.db.Exec("UPDATE parcel SET address = :address WHERE number = :number",
+		sql.Named("address", address),
+		sql.Named("number", number))
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
